2f0c9f90d629afe9b2df469ee1490c86431696df
Veysel Bozkurt <19290219@ogrenci.ankara.edu.tr>
2022-04-07 20:51:23 +0300

    Initial Commit

---
+++ KukaAppsiOSCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KukaAppsiOSCase
+//
+//  Created by Veysel Bozkurt on 7.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
